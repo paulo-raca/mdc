1723382588ea52131390afea1b5390660f589813
Paulo Costa <me@paulo.costa.nom.br>
2018-03-27 21:02:46 -0300

    [inf612] Adicionado enunciado do Teste 4

---
+++ inf612/teste4/teste4.R
@@ -0,0 +1,21 @@
+########################################
+# Teste 4 - INF-0612          
+# Nome(s): 
+########################################
+
+names <- c("Horario", "Temperatura", "Vento", "Umidade", "Sensacao")
+cepagri <- read.csv("cepagri.csv", header = FALSE, sep = ";", col.names = names)
+
+library(ggplot2)
+
+## 1 - Umidade Relativa do Ar
+
+
+## 2 - Sensacao Termica da Segunda Quinzena do Mes
+
+
+## 3 - Temperatura dos Ultimos Sete Dias do Mes
+
+
+## 4 - Ventos do Primeiro Dia do Mes
+
