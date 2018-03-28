4cedaf4edd3a876902a76abc27f8e40688217326
Paulo Costa <me@paulo.costa.nom.br>
2018-03-27 23:40:46 -0300

    [inf612] Implementado bonus do Teste4

--- inf612/teste4/teste4.R
+++ inf612/teste4/teste4.R
@@ -12,25 +12,25 @@ library(ggplot2)
 cepagri$Horario <- as.POSIXlt(strptime(cepagri$Horario, "%d/%m/%Y-%H:%M"))
 
 ## 0B Retuns a slice of cepagry dataframe between the days `from` and `to` (inclusive)
-cepagry_date_range <- function(from, to) {
+cepagri_date_range <- function(from, to) {
     cepagri[ is.element(cepagri$Horario$mday, from:to), ]
 }
 
 ## 1 - Umidade Relativa do Ar
 
-p <- ggplot(cepagry_date_range(1, 10), aes(x = Horario, y = Umidade))
+p <- ggplot(cepagri_date_range(1, 10), aes(x = Horario, y = Umidade))
 p <- p + geom_line()
 p
 
 ## 2 - Sensacao Termica da Segunda Quinzena do Mes
 
-p <- ggplot(cepagry_date_range(15, 31), aes(x = Sensacao))
+p <- ggplot(cepagri_date_range(15, 31), aes(x = Sensacao))
 p <- p + geom_histogram(bins = 30)
 p
 
 ## 3 - Temperatura dos Ultimos Sete Dias do Mes
 
-p <- ggplot(cepagry_date_range(25, 31), aes(
+p <- ggplot(cepagri_date_range(25, 31), aes(
     x = Horario$mday, 
     y = Temperatura, 
     group = Horario$mday))
@@ -39,8 +39,7 @@ p
 
 ## 4 - Ventos do Primeiro Dia do Mes
 
-
-cepagry_jan_1 <- cepagry_date_range(1, 1)
+cepagry_jan_1 <- cepagri_date_range(1, 1)
 max_wind_by_hour <- as.data.frame(tapply(cepagry_jan_1$Vento, cepagry_jan_1$Horario$hour, max))
 colnames(max_wind_by_hour)[1] <- 'Vento'
 max_wind_by_hour$Hora <- as.integer(row.names(max_wind_by_hour))
@@ -48,4 +47,12 @@ max_wind_by_hour$Hora <- as.integer(row.names(max_wind_by_hour))
 p <- ggplot(max_wind_by_hour, aes(x = Hora, y = Vento))
 p <- p + geom_point()
 p <- p + geom_smooth()
+p
+
+
+## 5 - Anomalia
+
+# O arquivo não possui dados entre 13/01/2018-09:30 e 14/01/2018-10:30
+p <- ggplot(cepagri_date_range(13, 14), aes(x = Horario, y = Sensacao))
+p <- p + geom_line()
 p
